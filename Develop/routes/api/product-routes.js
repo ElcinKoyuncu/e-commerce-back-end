4a2bdc263decd6a0af7dfc3273846b3650694700
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-06-08 22:41:26 -0500

    updating tag-routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -27,7 +27,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({ message: 'No reader found with that id!' });
+      res.status(404).json({ message: 'No product found with that id!' });
       return;
     }
 
